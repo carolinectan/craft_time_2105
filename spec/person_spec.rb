6f343b50682c9a9cfc89eb7fb95cb23a8fba1c92
Caroline Tan <carolinectan3@gmail.com>
2021-06-08 15:08:10 -0400

    Add tests for person

---
+++ spec/person_spec.rb
@@ -0,0 +1,27 @@
+require './lib/person'
+
+RSpec.describe Person do
+  before :each do
+    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+  end
+
+  it 'exists' do
+    expect(@person).to be_a(Person)
+  end
+
+  it 'initializes with attributes' do
+    expect(@person.name).to eq("Hector")
+    expect(@person.interests).to eq(["sewing", "millinery", "drawing"])
+    expect(@person.supplies).to eq({})
+  end
+
+  it 'can add supplies' do
+    @person.add_supply('fabric', 4)
+    @person.add_supply('scissors', 1)
+
+    expect(@person.supplies).to eq({"fabric"=>4, "scissors"=>1})
+
+    @person.add_supply('fabric', 3)
+
+    expect(@person.supplies).to eq({"fabric"=>7, "scissors"=>1})
+end
