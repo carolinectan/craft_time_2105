d85baf0581c36d2fc9e38001a7c694ab33d753d2
Caroline Tan <carolinectan3@gmail.com>
2021-06-08 16:30:53 -0400

    Add can build test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,4 +1,5 @@
 require './lib/person'
+require './lib/craft'
 
 RSpec.describe Person do
   before :each do
@@ -24,5 +25,22 @@ RSpec.describe Person do
     @person.add_supply('fabric', 3)
 
     expect(@person.supplies).to eq({"fabric"=>7, "scissors"=>1})
+  end
+
+  it 'can return if someone can build' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+
+    expect(hector.can_build?(sewing)).to eq(false)
+
+    hector.add_supply('fabric', 7)
+    hector.add_supply('thread', 1)
+
+    expect(hector.can_build?(sewing)).to eq(false)
+
+    hector.add_supply('scissors', 1)
+    hector.add_supply('sewing_needles', 1)
+    
+    expect(hector.can_build?(sewing)).to eq(true)
   end
 end
