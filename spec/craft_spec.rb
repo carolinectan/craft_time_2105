7bc5e0b767fa7e4a4f69a8f603c774de9a3c2eab
Caroline Tan <carolinectan3@gmail.com>
2021-06-08 15:08:23 -0400

    Add tests for craft

---
+++ spec/craft_spec.rb
@@ -0,0 +1,16 @@
+require './lib/craft'
+
+RSpec.describe Craft do
+  before :each do
+    @craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+  end
+
+  it 'exists' do
+    expect(@craft).to be_a(Craft)
+  end
+
+  it 'initializes with attributes' do
+    expect(@craft.name).to eq("knitting")
+    expect(@craft.supplies_required).to eq({:yarn=>20, :scissors=>1, :knitting_needles=>2})
+  end
+end
