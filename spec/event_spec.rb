7641b811f89362f0b0fd32f9c4ca89dbaf7fed2c
Caroline Tan <carolinectan3@gmail.com>
2021-06-08 16:31:31 -0400

    Add tests for most supplies and event supply list

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,11 +1,11 @@
-require './lib/event'
-require './lib/craft'
 require './lib/person'
+require './lib/craft'
+require './lib/event'
 
 RSpec.describe Event do
   before :each do
     @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
-    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    @craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
     @event = Event.new("Carla's Craft Connection", [@craft], [@person])
   end
 
@@ -17,5 +17,33 @@ RSpec.describe Event do
     expect(@event.name).to eq("Carla's Craft Connection")
     expect(@event.crafts).to eq([@craft])
     expect(@event.attendees).to eq([@person])
+  end
+
+  it 'can return attendee names' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+
+    expect(event.attendee_names).to eq(["Hector", "Toni"])
+  end
+
+  it 'can return craft with most supplies' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+
+    expect(event.craft_with_most_supplies).to eq("sewing")
+  end
+
+  it 'can return an event supply list' do
+    expect(@event.supply_list).to eq(["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"])
   end
 end
