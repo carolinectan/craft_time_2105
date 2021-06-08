dcdc349987b38bf4267a5dfd24c5dd7f1e7a9705
Caroline Tan <carolinectan3@gmail.com>
2021-06-08 16:32:01 -0400

    Draft craft with most supplies method

--- lib/event.rb
+++ lib/event.rb
@@ -6,4 +6,25 @@ class Event
     @crafts = crafts
     @attendees = attendees
   end
+
+  def attendee_names
+    @attendees.map do |person|
+      person.name
+    end
+  end
+
+  def craft_with_most_supplies
+    hash = @crafts.group_by do |craft|
+      craft.supplies_required
+    end
+
+    max_supplies_amount = hash.keys.map do |hash|
+      hash.values.sum
+    end.max
+
+    @crafts.select do |craft|
+      return craft.name if max_supplies_amount == craft.supplies_required.values.sum
+      # require "pry"; binding.pry
+    end
+  end
 end
