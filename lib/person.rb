fbff5ad06fc9b27f913b5e4cfe5ffb0f2752d584
Caroline Tan <carolinectan3@gmail.com>
2021-06-08 16:30:33 -0400

    Add can build test

--- lib/person.rb
+++ lib/person.rb
@@ -10,4 +10,20 @@ class Person
   def add_supply(supply, quantity)
     @supplies[supply] += quantity
   end
+
+  def can_build?(type)
+    supplies = type.supplies_required.keys.map do |sup|
+      sup.to_s
+    end
+
+    result = supplies.map do |supply|
+      @interests.include?(supply)
+    end
+# require "pry"; binding.pry
+    if @interests.any?(supplies)
+      true
+    else
+      false
+    end
+  end
 end
