615785f0b10533702bfdeb82c7378fb2b45a9732
Caroline Tan <carolinectan3@gmail.com>
2021-06-08 15:19:54 -0400

    Add tests for event

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,18 +1,21 @@
 require './lib/event'
+require './lib/craft'
+require './lib/person'
 
 RSpec.describe Event do
   before :each do
-    @event = Event.new("Carla's Craft Connection", [craft], [person])
+    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    @event = Event.new("Carla's Craft Connection", [@craft], [@person])
   end
 
   it 'exists' do
-    expect(@event).to be_a(Event)
+    expect(@event).to be_an(Event)
   end
 
   it 'initializes with attributes' do
     expect(@event.name).to eq("Carla's Craft Connection")
     expect(@event.crafts).to eq([@craft])
     expect(@event.attendees).to eq([@person])
-
   end
 end
