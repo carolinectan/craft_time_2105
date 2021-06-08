776c9d80bbaac9fb115c3e1901bae7974b99c339
Caroline Tan <carolinectan3@gmail.com>
2021-06-08 16:45:15 -0400

    Finish supply list method'

--- lib/event.rb
+++ lib/event.rb
@@ -21,10 +21,20 @@ class Event
     max_supplies_amount = hash.keys.map do |hash|
       hash.values.sum
     end.max
-
     @crafts.select do |craft|
       return craft.name if max_supplies_amount == craft.supplies_required.values.sum
-      # require "pry"; binding.pry
     end
+  end
+
+  def supply_list
+    arr_of_hashes = @crafts.map do |craft|
+      craft.supplies_required
+    end
+
+    arr_of_hashes.flat_map do |hash|
+      hash.keys.map do |key|
+        key.to_s
+      end
+    end.uniq
   end
 end
