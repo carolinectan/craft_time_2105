997c654e5c8bacb1c754f0b66248e3d8d3a7efcd
Caroline Tan <carolinectan3@gmail.com>
2021-06-09 08:32:47 -0400

    Break tests into iteration blocks

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,55 +3,59 @@ require './lib/craft'
 require './lib/event'
 
 RSpec.describe Event do
-  before :each do
-    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
-    @craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
-    @event = Event.new("Carla's Craft Connection", [@craft], [@person])
+  describe 'Iteration 1' do
+    before :each do
+      @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      @craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      @event = Event.new("Carla's Craft Connection", [@craft], [@person])
+    end
+
+    it 'exists' do
+      expect(@event).to be_an(Event)
+    end
+
+    it 'initializes with attributes' do
+      expect(@event.name).to eq("Carla's Craft Connection")
+      expect(@event.crafts).to eq([@craft])
+      expect(@event.attendees).to eq([@person])
+    end
   end
 
-  it 'exists' do
-    expect(@event).to be_an(Event)
-  end
+  describe 'Iteration 2' do
+    it 'can return attendee names' do
+      hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
 
-  it 'initializes with attributes' do
-    expect(@event.name).to eq("Carla's Craft Connection")
-    expect(@event.crafts).to eq([@craft])
-    expect(@event.attendees).to eq([@person])
-  end
+      sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
 
-  it 'can return attendee names' do
-    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
-    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+      event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
 
-    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
-    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      expect(event.attendee_names).to eq(["Hector", "Toni"])
+    end
 
-    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+    it 'can return craft with most supplies' do
+      hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
 
-    expect(event.attendee_names).to eq(["Hector", "Toni"])
-  end
+      sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
 
-  it 'can return craft with most supplies' do
-    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
-    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+      event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
 
-    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
-    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      expect(event.craft_with_most_supplies).to eq("sewing")
+    end
 
-    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+    it 'can return an event supply list' do
+      hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
 
-    expect(event.craft_with_most_supplies).to eq("sewing")
-  end
+      sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
 
-  it 'can return an event supply list' do
-    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
-    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+      event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
 
-    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
-    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
-
-    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
-
-    expect(event.supply_list).to eq(["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"])
+      expect(event.supply_list).to eq(["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"])
+    end
   end
 end
